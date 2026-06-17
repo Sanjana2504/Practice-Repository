9e939f97645a8f828998fcb63bfe368143309969
Bhavana Prasad <Bhavana Prasad@LAPTOP-M38JR7EH>
2026-06-17 14:33:28 +0530

    1st commit practice purpose

---
+++ Basicc/Demon.java
@@ -0,0 +1,12 @@
+package Basicc;
+class Demon
+{
+    public static void main(String[] args)
+    {
+        System.out.println(10);//10
+	    System.out.println(10.10);//10.10
+	    System.out.println("Hello");//Hello
+	    System.out.println('A');//A
+	    System.out.println(true);//true
+     }
+}    
---
+++ Basicc/Sample.java
@@ -0,0 +1,9 @@
+package Basicc;
+class Sample
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Java");
+	    //this program is printing a message
+    }
+}
